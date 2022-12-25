fb80fa49dd8d443516dcec0005fe79206a59cefb
Said Alimov <120774372+kindevelopment@users.noreply.github.com>
2022-12-25 10:55:23 +0500

    Добавил новые возможности admin-panel 'Поиск, фильтры'

--- support/admin.py
+++ support/admin.py
@@ -1,7 +1,12 @@
 
 from django.contrib import admin
+from django.contrib.admin.models import LogEntry
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from django.forms import BaseInlineFormSet
 from django.http import request
 
+from .forms import MessListForm
 from .models import Category, Message, UserMessage
 
 
@@ -20,6 +25,15 @@ class UserMessageInline(admin.StackedInline):
 class MessageAdmin(admin.ModelAdmin):
     list_display = ('subject', 'text', 'image', 'create_at', 'status', 'user')
     inlines = [UserMessageInline]
+    list_filter = ('create_at', 'status', 'category')
+    search_fields = ('user__username', )
+
+    def save_formset(self, request, form, formset, change):
+        instances = formset.save(commit=False)
+        for instance in instances:
+            instance.user = request.user
+            instance.save()
+        formset.save_m2m()
 
 
 @admin.register(UserMessage)
